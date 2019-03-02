8e90fa15fd4390a3df97dc826c440a12102bac7f
vackosar <admin@vaclavkosar.com>
2019-03-02 13:42:33 +0100

    compress to spx

--- urlspxget-server.sh
+++ urlspxget-server.sh
@@ -5,7 +5,9 @@ url="$1";
 name="$(echo "$url"|sed 's|.*/\([^?]*\)?*.*|\1|')";
 cat /dev/null |
   wget "$url" -O - |
-  ffmpeg -i pipe:0 -ac 1 -af "volume=1.5" -af lowpass=3000,highpass=300 -acodec libgsm_ms -ar 8000 -ab 13000 -f wav -y "$HOME/sync/$name.wav";
+  ffmpeg -i pipe:0 \
+    -ac 1 -af "volume=1.5" -af lowpass=3000,highpass=300 \
+    -acodec libspeex -ar 8000 -ab 4000 -vbr 1 -compression_level 10 -dtx 1 -f spx\
+    -y "$HOME/sync/$name.spx";
 
-# ffmpeg -i pipe:0 -acodec libspeex -ar 8000 -ab 4000 -vbr 1 -compression_level 10 -dtx 1 -ac 1 -af "volume=1.5" -y -f spx -"
-
+# -acodec libgsm_ms -ar 8000 -ab 13000 -f wav \
--- ytspxget-server.sh
+++ ytspxget-server.sh
@@ -14,5 +14,9 @@ sshToSrv() {
 
 name="$(youtube-dl --get-title "$url" |sed 's/[^a-zA-Z0-9]/-/g;'|cut -c 1-30)";
 cat /dev/null |youtube-dl --output - "$url" |
-  ffmpeg -i pipe:0 -ac 1 -af 'volume=1.5' -af lowpass=3000,highpass=300 -acodec libgsm_ms -ar 8000 -ab 13000  -f wav \
-  -y "$HOME/sync/$name.wav";
+  ffmpeg -i pipe:0 \
+    -ac 1 -af "volume=1.5" -af lowpass=3000,highpass=300 \
+    -acodec libspeex -ar 8000 -ab 4000 -vbr 1 -compression_level 10 -dtx 1 -f spx \
+    -y "$HOME/sync/$name.spx";
+
+# -acodec libgsm_ms -ar 8000 -ab 13000 -f wav \
