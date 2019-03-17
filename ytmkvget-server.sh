8fd7fb2c779cc4ef59c35abb1b824dca2ffc7b86
vackosar <admin@vaclavkosar.com>
2019-03-17 09:53:57 +0100

    vp9 n gsm

--- ytmkvget-server.sh
+++ ytmkvget-server.sh
@@ -15,6 +15,9 @@ sshToSrv() {
 name="$(youtube-dl --get-title "$url" |sed 's/[^a-zA-Z0-9]/-/g;'|cut -c 1-210)";
 cat /dev/null |youtube-dl --output - "$url" |
   ffmpeg -i pipe:0 \
-    -c:v libx264 -tune stillimage -crf 28 -r 1\
-    -c:a libspeex -ar 8000 -ab 4000 -ac 1 -af "volume=1.5" -af highpass=300 \
+    -c:v libvpx-vp9 -crf 63 -r 1 \
+    -c:a libgsm_ms -ar 8000 -ab 13000 -ac 1 -af "volume=1.5" \
     -y "$HOME/sync/$name.mkv";
+
+    #-c:v libx264 -tune stillimage -crf 28 -r 1\
+    #-c:a libspeex -ar 8000 -ab 4000 -ac 1 -af "volume=1.5" -af highpass=300 \
