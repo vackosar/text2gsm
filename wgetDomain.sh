342823d82603e53ee96742770468d9256c9a67f0
vackosar <admin@vaclavkosar.com>
2019-05-04 21:06:24 +0200

    wgetDomain fix

--- wgetDomain.sh
+++ wgetDomain.sh
@@ -3,4 +3,5 @@
 domain="$1"
 wget --recursive --convert-links "$domain";
 rm -r wp-* || true;
-ebook-convert "$domain/index.html" "$domain.epub";  
+protocolLess="$(echo "$domain"|sed 's|http[s]*://||')";
+ebook-convert "$protocolLess/index.html" "$domain.epub";  
