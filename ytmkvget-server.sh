bda92351db3c69ac9a0982210d263fbef7c4f860
vackosar <admin@vaclavkosar.com>
2019-05-18 19:23:12 +0200

    add ytspxget and mkv to use opus - better player support, quality, but larger.

--- ytmkvget-server.sh
+++ ytmkvget-server.sh
@@ -16,8 +16,9 @@ name="$(youtube-dl --get-title "$url" |sed 's/[^a-zA-Z0-9]/-/g;'|cut -c 1-210)";
 cat /dev/null |youtube-dl --output - "$url" |
   ffmpeg -i pipe:0 \
     -c:v libvpx-vp9 -crf 63 -r 1 \
-    -c:a libgsm_ms -ar 8000 -ab 13000 -ac 1 -af "volume=1.5" \
+    -c:a libopus -b:a 6K -ac 1 \
     -y "$HOME/sync/$name.mkv";
 
-    #-c:v libx264 -tune stillimage -crf 28 -r 1\
-    #-c:a libspeex -ar 8000 -ab 4000 -ac 1 -af "volume=1.5" -af highpass=300 \
+#-c:a libgsm_ms -ar 8000 -ab 13000 -ac 1 -af "volume=1.5" \
+#-c:v libx264 -tune stillimage -crf 28 -r 1\
+#-c:a libspeex -ar 8000 -ab 4000 -ac 1 -af "volume=1.5" -af highpass=300 \
