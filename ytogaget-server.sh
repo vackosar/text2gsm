279e827a8923f22c89c671268aafd4a65a71f81b
vackosar <admin@vaclavkosar.com>
2019-08-04 11:22:28 +0200

    opusfy, ytogaget

--- ytogaget-server.sh
+++ ytogaget-server.sh
@@ -15,9 +15,9 @@ sshToSrv() {
 name="$(youtube-dl --get-title "$url" |sed 's/[^a-zA-Z0-9]/-/g;'|cut -c 1-210)";
 cat /dev/null |youtube-dl --output - "$url" |
   ffmpeg -i pipe:0 \
-    -ac 1 -af "volume=1.1" \
-    -acodec libopus -b:a 6K -ac 1 -f opus \
-    -y "$HOME/sync/$name.opus";
+    -ac 1 -af "volume=1.0" \
+    -acodec libvorbis -ac 1 -ar 8000 -f oga \
+    -y "$HOME/sync/$name.oga";
 
 # -af lowpass=3000,highpass=300 -acodec libspeex -ar 8000 -ab 4000 -vbr 1 -compression_level 10 -dtx 1 -f spx \
 # -acodec libgsm_ms -ar 8000 -ab 13000 -f wav \
